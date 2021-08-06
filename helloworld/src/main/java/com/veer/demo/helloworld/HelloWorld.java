c50a761c2ef43af7c4142a2059db8ef3ccf13904
Veersha <Veersha@veersha-llt-427.mshome.net>
2021-08-06 14:39:56 +0530

    1St

---
+++ helloworld/src/main/java/com/veer/demo/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.veer.demo.helloworld;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class HelloWorld {
+
+  public static void main(final String[] args) {
+   
+
+    System.out.println("Hello, World Jenkins!");
+  }
+
+}
